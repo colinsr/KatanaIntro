90921c3f9a503bf7887b4bc19bb2ae06f18f1e42
colinsr <colinsr@me.com>
2015-06-11 10:27:21 -0400

    spike server

--- KatanaIntro/Program.cs
+++ KatanaIntro/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace KatanaIntro
+﻿using System;
+using Microsoft.Owin.Hosting;
+using Owin;
+
+namespace KatanaIntro
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string uri = "http://localhost:8080";
 
+            using (WebApp.Start<Startup>(uri))
+            {
+                Console.WriteLine("Server started on port 8080.");
+                Console.ReadKey();
+                Console.WriteLine("Server stopped");
+                Console.ReadKey();
+            }
+        }
+    }
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Run(ctx => ctx.Response.WriteAsync("Hello world!"));
         }
     }
 }
