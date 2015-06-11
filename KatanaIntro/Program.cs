0c8cf4a5ad51b8d95d08a295e0ffa1cd9fc403fe
colinsr <colinsr@me.com>
2015-06-11 11:33:37 -0400

    comment out main

--- KatanaIntro/Program.cs
+++ KatanaIntro/Program.cs
@@ -3,28 +3,27 @@ using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 using System.Web.Http;
-using Microsoft.Owin.Hosting;
 using Owin;
 
 namespace KatanaIntro
 {
     using AppFunc = Func<IDictionary<string, object>, Task>;
 
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string uri = "http://localhost:8080";
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        string uri = "http://localhost:8080";
 
-            using (WebApp.Start<Startup>(uri))
-            {
-                Console.WriteLine("Server started on port 8080.");
-                Console.ReadKey();
-                Console.WriteLine("Server stopped");
-                Console.ReadKey();
-            }
-        }
-    }
+    //        using (WebApp.Start<Startup>(uri))
+    //        {
+    //            Console.WriteLine("Server started on port 8080.");
+    //            Console.ReadKey();
+    //            Console.WriteLine("Server stopped");
+    //            Console.ReadKey();
+    //        }
+    //    }
+    //}
 
     public class Startup
     {
